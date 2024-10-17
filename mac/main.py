c4ea887f6600130ab7c32655c4c71852ee276307
Elias Z <eliaspublic@icloud.com>
2024-10-16 21:53:56 -0400

    stuff

--- mac/main.py
+++ mac/main.py
@@ -7,12 +7,12 @@ def is_brew_installed():
     except FileNotFoundError:
         return False
 
-def install_app_with_brew(app_name):
+def install_app_with_brew():
     """Install an app using Homebrew"""
     if is_brew_installed():
         try:
             subprocess.run("/pentoolkit/mac/install.sh", shell=True)
-        else:
+        finally:
             print("Homebrew is not installed. Please install Homebrew first. (Use {.href [this](https://github.com/Homebrew/brew/releases/download/4.4.1/Homebrew-4.4.1.pkg)} to quickly download it)")
 if __name__ == "__main__":
-    
+    install_app_with_brew()
