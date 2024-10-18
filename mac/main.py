428f147c088f5885fb14081ccab0cbafd97f8197
aptilco <zeinerer@icloud.com>
2024-10-18 14:00:37 -0400

    Revert "save"

--- mac/main.py
+++ mac/main.py
@@ -1,15 +1,15 @@
 import os
 import subprocess
 
-def install_permissions():
-    subprocess.run("chmod +x mac/run.sh", shell=True)
+def clear():
+    subprocess.run("clear", shell=True)
     
 def check_programs():
     pass # Check to see if the Brew programs are installed takes off 1 from the main menu, as a backup for the install.py
 
 def main_menu():
     while True:
-        subprocess.run("clear", shell=True)
+        clear()
         print("Main Menu:")
         print("1. Run Script")
         print("2. Run Install Script")
@@ -18,13 +18,12 @@ def main_menu():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            subprocess.run("clear", shell=True)
-            subprocess.run("mac/run.sh", shell=True)
+            pass
         elif choice == '2':
-            subprocess.run("clear", shell=True)
+            clear()
             subprocess.run("mac/install.py", shell=True)
         elif choice == '3':
-            subprocess.run("clear", shell=True)
+            clear()
             subprocess.run("mac/update.py", shell=True)
         elif choice == '4':
             print("Exiting...")
@@ -33,5 +32,5 @@ def main_menu():
             print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
-    subprocess.run("clear", shell=True)
+    clear()
     main_menu()
